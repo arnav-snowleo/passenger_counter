ca182cbad832178231f526e080a8fbadcfb23e09
arnav-snowleo <prakashsinghaarav@gmail.com>
2022-03-22 15:50:14 +0530

    increment, decrement, greet user from prompt i/p

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-document.getElementById("counter").innerText = 20;
+let count = 0;
+let x = document.getElementById("counter");
+let y = document.getElementById("greeting");
+// let variables are block scoped
+
+let greeting = "Welcome back, ";
+let userName = prompt("What's your name?");
+let greetingText = greeting + userName;
+y.innerText = greetingText;
+
+let savedarray = [];
+
+function save() {
+    console.log("save");
+    savedarray.push(10);
+    savedarray.push(20);
+    console.log(savedarray[0], savedarray[1]);
+}
+
+function increment() {
+    console.log("increment");
+    count++;
+    x.innerText = count;
+    console.log(count);
+}
+
+function reset() {
+    console.log("reset");
+    count = 0;
+    x.innerText = count;
+    console.log(count);
+}
+
+function decrement() {
+    console.log("decrement");
+    if (count > 0) {
+        count--;
+        x.innerText = count;
+        console.log(count);
+    }
+}

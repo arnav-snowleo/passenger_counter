7713ce3b9d9ad8fd21e76d3cd37f28c154921e0a
arnav-snowleo <prakashsinghaarav@gmail.com>
2022-03-22 16:19:24 +0530

    saved count array

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 let count = 0;
 let x = document.getElementById("counter");
 let y = document.getElementById("greeting");
+let z = document.getElementById("saved-counts");
 // let variables are block scoped
 
 let greeting = "Welcome back, ";
 let userName = prompt("What's your name?");
 let greetingText = greeting + userName;
 y.innerText = greetingText;
+y.innerText += " 👋";
 
 let savedarray = [];
 
 function save() {
     console.log("save");
-    savedarray.push(10);
-    savedarray.push(20);
-    console.log(savedarray[0], savedarray[1]);
+    savedarray.push(count);
+    for (let i = 0; i < savedarray.length; i++) {
+        // console.log(savedarray[i]);
+        z.innerText = savedarray;
+    }
 }
 
 function increment() {
